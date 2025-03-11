6d245ffab8e95d794c9f8f3c0a5847c3feed5982
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-03-11 18:59:04 +0500

    Completed Directives --Ahmed

--- src/app/Components/directives/directives.component.ts
+++ src/app/Components/directives/directives.component.ts
@@ -2,6 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  isAvailable: boolean;
+}
+
 @Component({
   selector: 'app-directives',
   imports: [CommonModule, FormsModule],
@@ -10,7 +18,13 @@ import { FormsModule } from '@angular/forms';
 })
 export class DirectivesComponent {
   isVisible: boolean = false;
-  names: string[] = ['John Doe', 'Jane Smith', 'Alice Johnson', 'Bob Brown'];
+  names: string[] = [
+    '',
+    'John Doe',
+    'Jane Smith',
+    'Alice Johnson',
+    'Bob Brown',
+  ];
 
   togglevisibility() {
     this.isVisible = !this.isVisible;
@@ -18,4 +32,39 @@ export class DirectivesComponent {
 
   num1: number = 12;
   num2: number = 12;
+
+  isActive: boolean = false;
+
+  selectedCountry: string = '';
+
+  peoples: Person[] = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      email: 'john.doe@example.com',
+      isAvailable: true,
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      age: 25,
+      email: 'jane.smith@example.com',
+      isAvailable: false,
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      age: 28,
+      email: 'alice.johnson@example.com',
+      isAvailable: true,
+    },
+    {
+      firstName: 'Bob',
+      lastName: 'Brown',
+      age: 35,
+      email: 'bob.brown@example.com',
+      isAvailable: true,
+    },
+  ];
 }

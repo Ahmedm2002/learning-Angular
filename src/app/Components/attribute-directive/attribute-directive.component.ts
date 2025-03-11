a5c55feeadfb1a7661336d3912a8dfea96b67cea
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-03-12 00:38:36 +0500

    Added home & page not found + half attribute directives --Ahmed

---
+++ src/app/Components/attribute-directive/attribute-directive.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  isAvailable: boolean;
+}
+
+@Component({
+  selector: 'app-attribute-directive',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './attribute-directive.component.html',
+  styleUrl: './attribute-directive.component.css',
+})
+export class AttributeDirectiveComponent {
+  div1BgColor: string = 'bg-info';
+
+  makeRed() {
+    this.div1BgColor = 'bg-danger';
+  }
+
+  makeYellow() {
+    this.div1BgColor = 'bg-warning';
+  }
+
+  isDivActive: boolean = true;
+  isActive: boolean = true;
+
+  peoples: Person[] = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      email: 'john.doe@example.com',
+      isAvailable: true,
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      age: 25,
+      email: 'jane.smith@example.com',
+      isAvailable: false,
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      age: 28,
+      email: 'alice.johnson@example.com',
+      isAvailable: true,
+    },
+    {
+      firstName: 'Bob',
+      lastName: 'Brown',
+      age: 35,
+      email: 'bob.brown@example.com',
+      isAvailable: true,
+    },
+  ];
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,15 @@ import { UserComponent } from './Components/user/user.component';
 import { AdminComponent } from './Components/admin/admin.component';
 import { DataBindingComponent } from './Components/data-binding/data-binding.component';
 import { DirectivesComponent } from './Components/directives/directives.component';
+import { AttributeDirectiveComponent } from './Components/attribute-directive/attribute-directive.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { HomeComponent } from './Components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'user',
     component: UserComponent,
@@ -20,5 +27,13 @@ export const routes: Routes = [
   {
     path: 'directives',
     component: DirectivesComponent,
+  },
+  {
+    path: 'attribute-directive',
+    component: AttributeDirectiveComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
